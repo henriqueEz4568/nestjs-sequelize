f42c7353c9a26ca5d88c7b6b252f8f505b8d1239
henriqueNectaco <henrique@nectaco.com.br>
2025-03-31 19:21:55 -0300

    fix config.js

--- src/infra/db/config.js
+++ src/infra/db/config.js
@@ -1,27 +1,29 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-import dotenv from 'dotenv';
-dotenv.config();
-export const development = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-  port: process.env.DB_PORT || 5432,
-};
-export const test = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-  port: process.env.DB_PORT || 5432,
-};
-export const production = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-  port: process.env.DB_PORT || 5432,
+import { config } from 'dotenv';
+config();
+
+export default {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: Number(process.env.DB_PORT),
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: Number(process.env.DB_PORT),
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: Number(process.env.DB_PORT),
+  },
 };
